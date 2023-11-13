0dd984a5520f50f8f6f2f3bb445870ed478c96c6
arunsrinivasan2 <arunsri1983@gmail.com>
2023-11-13 10:56:21 +0000

    mta build

---
+++ app/demoproject/dist/Component-dbg.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/fe/core/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("demoproject.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
---
+++ app/demoproject/dist/Component.js
@@ -0,0 +1,1 @@
+sap.ui.define(["sap/fe/core/AppComponent"],function(e){"use strict";return e.extend("demoproject.Component",{metadata:{manifest:"json"}})});
